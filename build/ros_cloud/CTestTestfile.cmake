69072a00462cda19bd5fbca1a6d7d11794f68ec5
whutseu_zhou <53174131+zsx-star@users.noreply.github.com>
2021-03-18 09:15:08 +0800

    .

--- build/ros_cloud/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/zsx/learning_ws/src/ros_cloud
-# Build directory: /home/zsx/learning_ws/build/ros_cloud
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
